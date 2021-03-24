8a6918190c7802710985afa97c26c9f23bebed78
sgsg <sgsg>
2021-03-24 21:42:39 +0800

    fasf

--- bin/daemon.js
+++ bin/daemon.js
@@ -1,14 +1,17 @@
 var request = require("request");
 // 执行任务
 function dotask () {
-    request("http://127.0.0.1:3000/routes/start", function (err, res, body) {
-        console.log(body);
-    });
+    if (new Date().getHours() == 5) {
+        // 5点钟，更新
+        request("http://127.0.0.1:3000/routes/start", function (err, res, body) {
+            console.log(body);
+        });
+    }
 }
 // 立即执行一次
 // setTimeout(function () {
 //     dotask();
 // }, 30000);
 
-// 24个小时执行一次
-setInterval(dotask, 1000 * 60 * 60 * 24);
+// 1个小时执行一次
+setInterval(dotask, 1000 * 60 * 60 * 1);
