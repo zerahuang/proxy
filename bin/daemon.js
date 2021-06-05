90ecf11f7e051d354dce0510cf2263c5545379c7
afaf <fsfs>
2021-06-05 21:58:15 +0800

    fafaf

--- bin/daemon.js
+++ bin/daemon.js
@@ -1,4 +1,22 @@
 var request = require("request");
+var os = require('os');
+var fs = require('fs');
+
+function getIPAdress () {
+    var interfaces = os.networkInterfaces();
+    for (var devName in interfaces) {
+        if (devName != 'eth0') {
+            var iface = interfaces[devName];
+            for (var i = 0; i < iface.length; i++) {
+                var alias = iface[i];
+                if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                    return alias.address;
+                }
+            }
+        }
+    }
+}
+
 // 执行任务
 function dotask () {
     if (new Date().getHours() == 5) {
@@ -10,11 +28,15 @@ function dotask () {
 }
 
 function doasync () {
-    request("http://127.0.0.1:3000/routes/asyncip", function (err, res, body) {
+    // request("http://127.0.0.1:3000/routes/asyncip", function (err, res, body) {
+    //     console.log("do async:", new Date().toLocaleString(), body);
+    // });
+    var link = "http://onhit.cn/sanpk/tools-updateip?ip=" + getIPAdress() + "&n=" + encodeURIComponent(fs.readFileSync('name.txt').toString().replace(/\n$/, '')) + "&w=" + (+fs.readFileSync('weight.txt').toString());
+    // console.log(link);
+    request(link, function (err, res, body) {
         console.log("do async:", new Date().toLocaleString(), body);
     });
 }
-
 // 立即执行一次
 // setTimeout(function () {
 //     dotask();
